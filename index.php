2b5416b6ab7e331e5497c35bb9fb999de501b0d4
Oxibug <agskitkat@gmail.com>
2020-01-05 15:16:31 +0300

    Init lesson

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+class FileExplorer {
+    function __construct() {
+
+    }
+
+    function render() {
+        include "explorer.php";
+    }
+}
+
+$explorer = new FileExplorer();
+$explorer->render();
+
+
