3aeb7c912bfebde0573e9e3f5c4af4820f53becd
Oxibug <agskitkat@gmail.com>
2020-01-11 16:22:34 +0300

    Update test.php

--- test.php
+++ test.php
@@ -6,37 +6,50 @@ for($i = 0; $i < $_GET['test']; $i++) {
     $array[$i] = true;
 }
 
+// Объект исследованиея
+$object = $_GET['type'] === "foreach"?:'spl';
 
-if ($_GET['type'] === "foreach") {
-    $interation = 0;
+// Операция
+$operation = $_GET['operation']?:'bruteforce';
 
-    $ta = microtime(true);
-    foreach ($array as $item) {
-        $interation++;
+// Выбор операции
+if($operation === "bruteforce" ) { // Перебор массива
+    if ($object === "foreach") {
+        // Обычное foreach
+        $interation = 0;
+        $ta = microtime(true);
+        foreach ($array as $item) {
+            $interation++;
+        }
+        $te = microtime(true);
+
+        $ar['Time'] = $te - $ta;
+        $ar['Type'] = "FOREACH";
+    } else {
+        // Обычное SPL
+        $interation = 0;
+        $obj = new ArrayObject($array);
+        $iter = $obj->getIterator();
+        $ta2 = microtime(true);
+        while ($iter->valid()) {
+            $interation++;
+            $iter->next();
+        }
+        $te2 = microtime(true);
+        $ar['Time'] = ($te2 - $ta2);
+        $ar['Type'] = "SPL";
     }
-    $te = microtime(true);
 
-    $ar['Time'] = $te - $ta;
-    $ar['Type'] = "FOREACH";
 
-} else {
-    $interation = 0;
-    $obj = new ArrayObject($array);
-    $iter = $obj->getIterator();
+} elseif ($operation === "find" ) { // Доступ к элементам
 
-    $ta2 = microtime(true);
+} elseif ($operation === "add" ) {  // Добавление элементов
 
-    while ($iter->valid()) {
-        $interation++;
-        $iter->next();
-    }
-
-    $te2 = microtime(true);
-
-    $ar['Time'] = ($te2 - $ta2);
-    $ar['Type'] = "SPL";
+} elseif ($operation === "remove" ) { // Удаление элементов
 
 }
+
+
 $ar['Time'] = round($ar['Time'], 10);
 $ar['memory'] = memory_get_usage() / 100000;
 echo json_encode($ar);
